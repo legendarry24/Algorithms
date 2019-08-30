06494eda7b147bce3acace178deb53c01f63ed72
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2019-08-31 02:37:24 +0300

    BinarySearch generic implementation

---
+++ BinarySearch/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BinarySearch
+{
+	public static class Extensions
+	{
+		public static int BinarySearch<T>(this IEnumerable<T> source, T item, IComparer<T> comparer = null)
+		{
+			var array = source as T[] ?? source.ToArray();
+			comparer = comparer ?? Comparer<T>.Default;
+
+			int min = 0;
+			int max = array.Length - 1;
+
+			while (min <= max)
+			{
+				int mid = (min + max) / 2;
+
+				if (item.Equals(array[mid]))
+				{
+					return ++mid;
+				}
+
+				if (comparer.Compare(item, array[mid]) > 0)
+				{
+					min = mid + 1;
+				}
+				else
+				{
+					max = mid - 1;
+				}
+			}
+
+			return -1;
+		}
+
+		public static int BinarySearch(this int[] array, int item)
+		{
+			int min = 0;
+			int max = array.Length - 1;
+
+			while (min <= max)
+			{
+				int mid = (min + max) / 2;
+
+				if (item == array[mid])
+				{
+					return ++mid;
+				}
+
+				if (item > array[mid])
+				{
+					min = mid + 1;
+				}
+				else
+				{
+					max = mid - 1;
+				}
+			}
+
+			return -1;
+		}
+	}
+}
---
+++ BinarySearch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var array = new[] { 1.1, 3.2, 5.3, 7.4, 9.5 };
+			var strArray = new[] { "bar", "baz", "foo" };
+
+			Console.WriteLine(array.BinarySearch(3.2));
+			Console.WriteLine(strArray.BinarySearch("foo"));
+
+			var source = ParseInputString(Console.ReadLine().Split(' '));
+			var items = ParseInputString(Console.ReadLine().Split(' '));
+
+			foreach (int item in items)
+			{
+				Console.Write($"{source.BinarySearch(item)} ");
+			}
+		}
+
+		static int[] ParseInputString(IReadOnlyList<string> input)
+		{
+			int length = int.Parse(input[0]);
+			var array = new int[length];
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				array[i] = int.Parse(input[i + 1]);
+			}
+
+			return array;
+		}
+	}
+}
