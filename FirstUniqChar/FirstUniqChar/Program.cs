fc5a7ce0917f896a8dd455a75eb6cc996377bff0
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2022-08-15 20:52:19 +0300

    First Unique Character in a String

---
+++ FirstUniqChar/FirstUniqChar/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace FirstUniqChar
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(GetFirstUniqChar("aabbcd")); // output: 4
+		}
+
+		private static int GetFirstUniqChar(string input)
+		{
+			if (input.Length == 1)
+			{
+				return 0;
+			}
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				char currentChar = input[i];
+
+				//bool matchFound = false;
+
+				//for (int j = 0; j < input.Length; j++)
+				//{
+				//	if (currentChar == input[j] && i != j)
+				//	{
+				//		matchFound = true;
+				//		break;
+				//	}
+				//}
+
+				// LINQ version
+				bool matchFound = input.Where((c, j) => currentChar == c && i != j).Any();
+
+				if (!matchFound)
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+	}
+}
