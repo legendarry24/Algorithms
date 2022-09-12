98e050ff1d6bbf511dee826c0d905d3e88a0f0f3
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2022-09-12 20:28:47 +0300

    Find the Missing Number - implemented approach using the summation of the first N natural numbers

---
+++ FindMissingNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FindMissingNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// input array contains 'N' numbers between 1 and 'N+1'
+			// with one number missing and no duplicates
+			int[] array = { 3, 2, 4, 7, 1, 5 };
+
+			Console.WriteLine("The missing element is " + FindMissingNumber(array));
+		}
+
+		private static int FindMissingNumber(IReadOnlyCollection<int> array)
+		{
+			// the range of numbers is 1 more than the size of the array
+			int n = array.Count + 1;
+
+			// the sum of the first N natural numbers
+			int total = n * (n + 1) / 2;
+
+			// calculate the sum of all the array elements
+			int sum = array.Sum();
+
+			// expected sum - actual sum
+			return total - sum;
+		}
+	}
+}
