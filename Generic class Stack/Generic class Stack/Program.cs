6bd3beedbaf38385a61540d43451247b8f7342ba
Igor Olkhovskiy <ihor.olkhovskyi@nure.ua>
2022-09-26 01:14:31 +0300

    Added a Generic class Stack implementation

---
+++ Generic class Stack/Generic class Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Generic_class_Stack
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			var intStack = new Stack<int>(3);
+			
+			intStack.Push(1);
+			intStack.Push(2);
+			intStack.Push(3);
+			Console.WriteLine("Добавили 3 элемента:");
+			intStack.Show();
+			Console.WriteLine("Третий элемент = {0}", intStack.Get(2));
+			intStack.Pop();
+			intStack.Pop();
+			Console.WriteLine("Удалили 2 элемента:");
+			intStack.Show();
+
+			var strStack = new Stack<string>(5);
+
+			strStack.Push("First");
+			strStack.Push("Second");
+			Console.WriteLine("Добавили 2 элемента:");
+			strStack.Show();
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Generic class Stack/Generic class Stack/Stack.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Generic_class_Stack
+{
+	internal class Stack<T>
+	{
+		private readonly T[] _array;
+
+		private int _index = 0;
+
+		public Stack(int size)
+		{
+			_array = new T[size];
+		}
+
+		public void Push(T item)
+		{
+			_array[_index++] = item;
+		}
+
+		public T Pop()
+		{
+			return _array[--_index];
+		}
+
+		public T Get(int k)
+		{
+			return _array[k];
+		}
+
+		public void Show()
+		{
+			for (int i = 0; i < _index; i++)
+			{
+				Console.WriteLine(_array[i]);
+			}
+		}
+	}
+}
